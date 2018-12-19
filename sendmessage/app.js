d680e36641f69fa0fe840660024765b81fa14bf9
diegogm <diegogm@amazon.com>
2018-12-19 10:46:41 -0500

    Pushing the forEach of each callback to a promise and resolving it so we can close #2

--- sendmessage/app.js
+++ sendmessage/app.js
@@ -1,58 +1,60 @@
 // Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-var AWS = require('aws-sdk');
+var AWS = require("aws-sdk");
 AWS.config.update({ region: process.env.AWS_REGION });
 var DDB = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
 
-require('aws-sdk/clients/apigatewaymanagementapi');
+require("aws-sdk/clients/apigatewaymanagementapi");
 
-exports.handler = function (event, context, callback) {
+exports.handler = function(event, context, callback) {
   var scanParams = {
     TableName: process.env.TABLE_NAME,
     ProjectionExpression: "connectionId"
   };
 
-  DDB.scan(scanParams, function (err, data) {
+  DDB.scan(scanParams, function(err, data) {
     if (err) {
-      callback(null, {
-        statusCode: 500,
-        body: JSON.stringify(err)
-      });
+      callback(null, {statusCode: 500,body: JSON.stringify(err)});
     } else {
       var apigwManagementApi = new AWS.ApiGatewayManagementApi({
         apiVersion: "2018-11-29",
-        endpoint: event.requestContext.domainName + "/" + event.requestContext.stage
+        endpoint:
+          event.requestContext.domainName + "/" + event.requestContext.stage
       });
-      var postParams = {
-        Data: JSON.parse(event.body).data
-      };
-      var count = 0;
-
-      data.Items.forEach(function (element) {
-        postParams.ConnectionId = element.connectionId.S;
-        apigwManagementApi.postToConnection(postParams, function (err) {
-          if (err) {
-            // API Gateway returns a status of 410 GONE when the connection is no
-            // longer available. If this happens, we simply delete the identifier
-            // from our DynamoDB table.
-            if (err.statusCode === 410) {
-              console.log("Found stale connection, deleting " + postParams.connectionId);
-              DDB.deleteItem({ TableName: process.env.TABLE_NAME,
-                               Key: { connectionId: { S: postParams.connectionId } } });
-            } else {
-              console.log("Failed to post. Error: " + JSON.stringify(err));
-            }
-          } else {
-            count++;
-          }
-        });
-      });
+      var postParams = {Data: JSON.parse(event.body).data};
+      let callbackArray = data.Items.map( async(el) => {postNotifications(apigwManagementApi, postParams, el)});
+      Promise.all(callbackArray);
 
       callback(null, {
         statusCode: 200,
-        body: "Data send to " + count + " connection" + (count === 1 ? "" : "s")
+        body: "Data sent."
       });
     }
   });
 };
+
+function postNotifications(api, postparams, el){
+  var localPostParam = postparams;
+  localPostParam.ConnectionId = el.connectionId.S;
+  api.postToConnection(localPostParam, function(err) {
+    if (err) {
+      // API Gateway returns a status of 410 GONE when the connection is no
+      // longer available. If this happens, we simply delete the identifier
+      // from our DynamoDB table.
+      if (err.statusCode === 410) {
+        console.log(
+          "Found stale connection, deleting " + localPostParam.connectionId
+        );
+        DDB.deleteItem({
+          TableName: process.env.TABLE_NAME,
+          Key: { connectionId: { S: localPostParam.connectionId } }
+        });
+      } 
+      else {
+        console.log("Failed to post. Error: " + JSON.stringify(err));
+      }
+    }
+    return true;
+  });
+}
