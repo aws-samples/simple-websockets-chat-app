f06d543283b058ff635410313c8c4b2df5a2c602
Chris Holmes <chlmes@amazon.com>
2020-04-30 18:54:37 -0600

    demo-ready

--- lambda/register/app.js
+++ lambda/register/app.js
@@ -6,14 +6,24 @@ const ddb = new AWS.DynamoDB.DocumentClient({
   region: process.env.AWS_REGION
 });
 const { TABLE_NAME, DEBUG } = process.env;
-// const DEBUG = true
+// let endpoint, event
+// // if (DEBUG) {
+// endpoint = "jhdk924ki7.execute-api.us-east-1.amazonaws.com/Prod/"
+// } else {
+//   endpoint = event.requestContext.domainName + "/" + event.requestContext.stage
+// }
 
-async function getAdminConnId() {
+// const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+//   apiVersion: "2018-11-29",
+//   endpoint: endpoint
+// });
+
+async function getConnId(uuid) {
   var params = {
-    KeyConditionExpression: 'PartitionKey = :role AND begins_with ( SortKey , :connid )',
+    KeyConditionExpression: 'PartitionKey = :uuid AND begins_with(SortKey, :role)',
     ExpressionAttributeValues: {
-      ':role': "role:ADMIN",
-      ':connid': "cid"
+      ':uuid': `uuid:${uuid}`,
+      ':role': "role"
     },
     TableName: TABLE_NAME
   };
@@ -21,106 +31,165 @@ async function getAdminConnId() {
   results = await ddb
     .query(params)
     .promise();
-  //console.log("AdminID: ", results.Items[0].cid)
+  // console.log("AdminID: ", results.Items)
   return results.Items[0]
 }
+// const DEBUG = true
 
-async function getCurrentSessions(adminId, userConnId) {
-  console.log(adminId, userConnId)
+// async function getAdminConnId() {
+//   var params = {
+//     KeyConditionExpression: 'PartitionKey = :role AND begins_with ( SortKey , :connid )',
+//     ExpressionAttributeValues: {
+//       ':role': "role:ADMIN",
+//       ':connid': "cid"
+//     },
+//     TableName: TABLE_NAME
+//   };
+
+//   results = await ddb
+//     .query(params)
+//     .promise();
+//   //console.log("AdminID: ", results.Items[0].cid)
+//   return results.Items[0]
+// }
+
+// async function getCurrentSessions(adminId, userConnId) {
+//   console.log(adminId, userConnId)
+//   var params = {
+//     KeyConditionExpression: 'PartitionKey = :userid AND SortKey = :adminid',
+//     ExpressionAttributeValues: {
+//       ':userid': `wsid:${userConnId}`,
+//       ':adminid': `wsid:${adminId.cid}`
+//     },
+//     TableName: TABLE_NAME
+//   };
+//   result = await ddb
+//     .query(params)
+//     .promise();
+//   console.log("results:", result)
+//   return result
+// }
+
+// async function addAdminSession(adminData, userConnId) {
+
+//   let sessions = await getCurrentSessions(adminData, userConnId)
+//   time = Date.now()
+//   console.log(time)
+//   console.log("addAdminSession", "no current sessions found")
+//   item = {
+//     PartitionKey: `awsid:${adminData.cid}`,
+//     SortKey: `cwsid:${userConnId}`,
+//     cid: adminData.SortKey,
+//     timestamp: time,
+//   }
+//   const putParams = {
+//     TableName: process.env.TABLE_NAME,
+//     Item: item,
+//   };
+//   await ddb.put(putParams).promise();
+// }
+
+// async function addUserSession(event, adminId, userData) {
+//   const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+//     apiVersion: "2018-11-29",
+//     endpoint: event.requestContext.domainName + "/" + event.requestContext.stage
+//   });
+
+//   let payload = JSON.stringify({
+//     event: "connchange",
+//     userdata: userData
+//   });
+//   if (!DEBUG) {
+//     await apigwManagementApi
+//       .postToConnection({ ConnectionId: adminId, Data: payload })
+//       .promise();
+//   }
+// }
+
+async function getHistory(uuid) {
   var params = {
-    KeyConditionExpression: 'PartitionKey = :userid AND SortKey = :adminid',
+    TableName: TABLE_NAME,
+    FilterExpression: "begins_with(SortKey, :msg)",
+
     ExpressionAttributeValues: {
-      ':userid': `wsid:${userConnId}`,
-      ':adminid': `wsid:${adminId.cid}`
-    },
-    TableName: TABLE_NAME
+      ":msg": "msg:",
+    }
   };
-  result = await ddb
-    .query(params)
+  // let params = {
+  //   KeyConditionExpression: 'PartitionKey = :role AND begins_with ( SortKey , :connid )',
+  //   ExpressionAttributeValues: {
+  //     ':role': "role:ADMIN",
+  //     ':connid': "cid"
+  //   },
+  //   TableName: TABLE_NAME
+  // };
+  connId = await getConnId('admin')
+  console.log(connId.cid)
+  results = await ddb
+    .scan(params)
     .promise();
-  console.log("results:", result)
-  return result
-}
+  //console.log("AdminID: ", results.Items[0].cid)
 
-async function addAdminSession(adminData, userConnId) {
-
-  let sessions = await getCurrentSessions(adminData, userConnId)
-  time = Date.now()
-  console.log(time)
-  console.log("addAdminSession", "no current sessions found")
-  item = {
-    PartitionKey: `awsid:${adminData.cid}`,
-    SortKey: `cwsid:${userConnId}`,
-    cid: adminData.SortKey,
-    timestamp: time,
-  }
-  const putParams = {
-    TableName: process.env.TABLE_NAME,
-    Item: item,
-  };
-  await ddb.put(putParams).promise();
-}
-
-async function addUserSession(event, adminId, userData) {
-  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-    apiVersion: "2018-11-29",
-    endpoint: event.requestContext.domainName + "/" + event.requestContext.stage
-  });
-
-  let payload = JSON.stringify({
-    event: "connchange",
-    userdata: userData
-  });
-  if (!DEBUG) {
-    await apigwManagementApi
-      .postToConnection({ ConnectionId: adminId, Data: payload })
+  for (i = 0; i < results.Items.length; i++) {
+    payload = results.Items[i].message
+    console.log(payload)
+    apigwManagementApi
+      .postToConnection({ ConnectionId: connId.cid, Data: JSON.stringify(payload) })
       .promise();
   }
 }
+async function sendMessages(payload) {
 
+}
 exports.handler = async event => {
+  //await getHistory("admin")
   let connectionData;
   let item;
   let adminData
-
-  const putParams = {
-    TableName: process.env.TABLE_NAME,
-  };
-  try {
-    adminData = await getAdminConnId()
-  } catch (err) { return err }
-  console.log("AdminData", adminData)
-  // If user sends valid JWT token we list them as an admin, otherwise we add them as a user.
   let items = []
   try {
-    decodedToken = jwt.decode(JSON.parse(event.body).token);
+    const decodedToken = jwt.decode(JSON.parse(event.body).token);
     console.log("Admin Registration Detected: ", decodedToken)
-    items = [{
-      PartitionKey: `uuid:admin`,
-      SortKey: `role:admin`,
-      cid: event.requestContext.connectionId,
-      uuid: `uuid:${decodedToken.sub}`
-    },
-    {
-      PartitionKey: `uuid:${decodedToken.sub}`,
-      SortKey: `role:admin`,
-      cid: event.requestContext.connectionId,
-    }]
+    await ddb.put({
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        PartitionKey: `uuid:admin`,
+        SortKey: `role:admin`,
+        cid: event.requestContext.connectionId,
+        uuid: `${decodedToken.sub}`
+      }
+    }).promise()
+    await ddb.put({
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        PartitionKey: `uuid:${decodedToken.sub}`,
+        SortKey: `role:admin`,
+        cid: event.requestContext.connectionId,
+      }
+    }).promise()
   } catch (err) {
     console.log(err)
     data = JSON.parse(event.body)
     console.log(data)
-    items = [{
-      PartitionKey: `uuid:${data.userdata.uuid}`,
-      name: data.userdata.name,
-      cid: event.requestContext.connectionId,
-      SortKey: `role:user`,
-    }]
+    await ddb.put({
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        PartitionKey: `uuid:${data.userdata.uuid}`,
+        name: data.userdata.name,
+        cid: event.requestContext.connectionId,
+        SortKey: `role:user`,
+      }
+    }).promise()
   }
-  items.map(item => {
-    putParams.Item = item
-    ddb.put(putParams).promise();
-  })
+
+  // const res = await items.map(async function (item) {
+  //   // console.log(items)
+  //   const putParams = { TableName: process.env.TABLE_NAME, Item: item }
+  //   console.log(putParams)
+  //   return await ddb.put(putParams).promise()
+
+  // })
+  // console.log(res)
   return { statusCode: 200, body: "Connected." };
 };
 
--- lambda/sendmessage/app.js
+++ lambda/sendmessage/app.js
@@ -6,6 +6,7 @@ Msg -> Admin:
   src: {
     connId: xxxx
     uuid: xxxxxx-xxxxxx-xxxxxx
+    userdata:
   }
   dest: {
     connId:
@@ -18,35 +19,47 @@ Msg -> Admin:
 */
 const AWS = require("aws-sdk");
 
+
+const DEBUG = process.env.AWS_REGION
+// const DEBUG = false
+const { TABLE_NAME } = process.env;
+
 const ddb = new AWS.DynamoDB.DocumentClient({
   apiVersion: "2012-08-10",
   region: process.env.AWS_REGION
 });
-const DEBUG = process.env.AWS_REGION
-const { TABLE_NAME } = process.env;
 
-async function storeMessage(uuid, message) {
-  const now = new Date();
-  console.log(now)
+
+// if (DEBUG) {
+endpoint = "jhdk924ki7.execute-api.us-east-1.amazonaws.com/Prod/"
+// } else {
+//   endpoint = event.requestContext.domainName + "/" + event.requestContext.stage
+// }
+const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+  apiVersion: "2018-11-29",
+  endpoint: endpoint,
+});
+
+async function storeMessage(uuid, message, ts) {
+
+  // if (DEBUG) { console.log(now) }
   const putParams = {
     TableName: process.env.TABLE_NAME,
     Item: {
       PartitionKey: `uuid:${uuid}`,
       message: message,
-      SortKey: `ts:${now.toISOString()}`,
+      SortKey: `msg:${ts}`,
     }
   };
-
-
-  ddb.put(putParams).promise();
+  console.log(await ddb.put(putParams).promise());
 }
 
-async function getAdminConnId() {
+async function getConnId(uuid) {
   var params = {
-    KeyConditionExpression: 'PartitionKey = :uuid AND SortKey = :role',
+    KeyConditionExpression: 'PartitionKey = :uuid AND begins_with(SortKey, :role)',
     ExpressionAttributeValues: {
-      ':uuid': "uuid:admin",
-      ':role': "role:admin"
+      ':uuid': `uuid:${uuid}`,
+      ':role': "role"
     },
     TableName: TABLE_NAME
   };
@@ -59,72 +72,39 @@ async function getAdminConnId() {
 }
 
 exports.handler = async event => {
-  /*
-    inbound: dest, text
-    outbound: source, text
-  */
-  if (DEBUG) {
-    endpoint = "jhdk924ki7.execute-api.us-east-1.amazonaws.com/Prod/"
-  } else {
-    endpoint = event.requestContext.domainName + "/" + event.requestContext.stage
-  }
-  adminId = await getAdminConnId()
-  console.log(adminId.cid)
-  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-    apiVersion: "2018-11-29",
-    endpoint: endpoint,
-  });
-  console.log(event.body)
-  const postData = JSON.parse(event.body);
-  if (!("userdata" in postData)) {
-    postData.userdata = {
-      uuid: null,
-      name: {
-        first: null,
-        last: null
-      }
-    }
-  }
-  postData.userdata.connId = event.requestContext.connectionId
-  console.log(postData)
+  // const adminId = await getConnId("admin")
+  console.log(JSON.parse(event.body))
+  const timestamp = new Date().toISOString();
 
-  let payload = JSON.stringify({
-    message: postData,
-    source: event.requestContext.connectionId
-  });
-  dest = ("dest" in postData) ? postData.dest : adminId.cid
-  storeMessage(postData.userdata.uuid, postData)
 
+  adminId = await getConnId("admin")
+
+  const payload = JSON.parse(event.body).payload;
+  if (!payload.src) {
+    payload.src = {}
+  }
+  payload.ts = timestamp
+  payload.src.connId = event.requestContext.connectionId
+
+  let uuid = payload.src.uuid === adminId.uuid ? payload.dest.uuid : payload.src.uuid
+
+  storeMessage(uuid, payload, timestamp)
+
+  console.log("new payload", payload)
   try {
     await apigwManagementApi
-      .postToConnection({ ConnectionId: dest, Data: JSON.stringify(postData) })
+      .postToConnection({ ConnectionId: payload.dest.connId, Data: JSON.stringify(payload) })
       .promise();
   } catch (err) {
-    console.log(err)
+    let destUuid = await getConnId(payload.dest.uuid)
+
+
+    let resp = await apigwManagementApi
+      .postToConnection({ ConnectionId: destUuid.cid, Data: JSON.stringify(payload) })
+      .promise();
+    console.log(resp)
   }
+
 
   return { statusCode: 200, body: "Data sent." };
 };
-
-  // const postCalls = connectionData.Items.map(async ({ connectionId }) => {
-  //   try {
-  // await apigwManagementApi
-  //   .postToConnection({ ConnectionId: connectionId, Data: payload })
-  //   .promise();
-  //   } catch (e) {
-  //     if (e.statusCode === 410) {
-  //       console.log(`Found stale connection, deleting ${connectionId}`);
-  //       await ddb
-  //         .delete({ TableName: TABLE_NAME, Key: { connectionId } })
-  //         .promise();
-  //     } else {
-  //       throw e;
-  //     }
-  //   }
-  // });
-
-  // try {
-  //   await Promise.all(postCalls);
-  // } catch (e) {
-  //   return { statusCode: 500, body: e.stack };
-  // }
