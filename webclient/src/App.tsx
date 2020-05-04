bdc04743611a8156727dc2ec6aff35b0227177b0
jonathanmv <jonathan.morales@jimdo.com>
2020-05-04 23:00:17 +0200

    chore: initial skeleton for webclient

---
+++ webclient/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import './App.css'
+
+export default class App extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div>
+        <h1>Hello World from TypeScript! 📦 🚀</h1>
+      </div>
+    )
+  }
+}
