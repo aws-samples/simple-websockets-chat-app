6d754391ff025df215a75393e764843e71110dbf
jonathanmv <jonathan.morales@jimdo.com>
2020-05-10 11:14:44 +0200

    refactor: onconnect, use event api to share conn count change

--- lambdas/onconnect/index.js
+++ lambdas/onconnect/index.js
@@ -1,9 +1,17 @@
-const { saveConnectionInRoom } = require("../api/room");
+const { saveConnectionInRoom, getConnectionIdsByRoomId } = require("../api/room");
+const { buildEvent, emitEvent, EventTypes } = require("../api/event");
 
 module.exports = async event => {
   const roomId = event.queryStringParameters.j;
   const connectionId = event.requestContext.connectionId;
   await saveConnectionInRoom(connectionId, roomId);
+
+  const connectionIds = await getConnectionIdsByRoomId(roomId);
+  const connectionsCount = connectionIds.length;
+  const eventType = EventTypes.CONNECTIONS_COUNT_CHANGED;
+  const data = { roomId, connectionsCount };
+  const systemEvent = buildEvent(eventType, data);
+  await emitEvent(event.requestContext, systemEvent, connectionIds);
 
   return { statusCode: 200, body: "Connected." };
 };
