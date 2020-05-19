85f76ee1ac3338c2289610ef8212c642833d9cb6
jonathanmv <jonathan.morales@jimdo.com>
2020-05-19 08:38:04 +0200

    chore: play sound onmessage if tab is hidden

--- webclient/src/components/Chat.tsx
+++ webclient/src/components/Chat.tsx
@@ -4,12 +4,14 @@ import { RoomContext } from '../context/roomContext'
 
 import Messages from './Messages'
 import TextBox from './TextBox'
+import { PlaySoundOnNewMessage } from './PlaySoundOnNewMessage';
 
 const Chat: React.FC = () => {
   const { authorId, messages } = React.useContext(RoomContext);
 
   return (
     <>
+      <PlaySoundOnNewMessage />
       <Messages messages={messages} authorId={authorId} />
       <TextBox />
     </>
