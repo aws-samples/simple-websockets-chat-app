d2ccf221f2723056abdb75bea3179473aaf71e47
jonathanmv <jonathan.morales@jimdo.com>
2020-06-02 08:01:56 +0200

    feat: render null if !canToggleOptions

--- webclient/src/components/OptionsToggle.tsx
+++ webclient/src/components/OptionsToggle.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import '../styles/OptionsToggle.css'
 
 import { clsn } from '../helpers/color'
+import { ChatFeaturesContext } from '../context/chatFeaturesContext'
 
 interface Props {
   active?: boolean;
@@ -9,11 +10,17 @@ interface Props {
   onClick: (e: React.MouseEvent) => void;
 }
 
-const OptionsToggle: React.FC<Props> = ({ active, onClick, inverted }) => (
+const OptionsToggle: React.FC<Props> = ({ active, onClick, inverted }) => {
+  const { canToggleOptions } = React.useContext(ChatFeaturesContext);
+  if (!canToggleOptions) {
+    return null;
+  }
+
+  return (
   <a href="#" className={clsn("txt circle", active && "active", inverted && 'dark')}
     onClick={e => {e.preventDefault(); onClick && onClick(e)}}
   >
   </a>
-);
+);}
 
 export default OptionsToggle
