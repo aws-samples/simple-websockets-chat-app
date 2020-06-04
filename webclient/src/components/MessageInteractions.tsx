4d0f88b10da6379f4cdb4ead324bbc6b242771da
jonathanmv <jonathan.morales@jimdo.com>
2020-06-04 20:20:39 +0200

    wip: show interactions only on selectedMessage

--- webclient/src/components/MessageInteractions.tsx
+++ webclient/src/components/MessageInteractions.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react'
 import '../styles/MessageInteractions.styl'
 import { Message } from '../interfaces'
+import { clsn } from '../helpers/color';
 
 interface Props {
   message: Message;
+  reverse?: boolean;
 }
 
-export const MessageInteractions: React.FC<Props> = () => {
+export const MessageInteractions: React.FC<Props> = ({ reverse }) => {
   return (
-    <div className="message-interactions">
-      
+    <div className={clsn("message-interactions", reverse && 'reverse')}>
+      <button>emoji</button>
+      <button>reply</button>
+      <button>delete</button>
     </div>
   );
 }
--- webclient/src/components/Messages.tsx
+++ webclient/src/components/Messages.tsx
@@ -9,9 +9,10 @@ import { MessageInteractions } from './MessageInteractions';
 interface Props {
   authorId: string;
   messages: Message[];
+  selectedMessage?: Message;
 }
 
-const Messages: React.FC<Props> = ({ authorId, messages }) => {
+const Messages: React.FC<Props> = ({ authorId, messages, selectedMessage }) => {
   return (
     <div className="messagesContainer">
       <ul className="messages">
@@ -20,15 +21,20 @@ const Messages: React.FC<Props> = ({ authorId, messages }) => {
           const useDark = shouldUseDark(backgroundColor);
           const style = { backgroundColor };
           const className = clsn("messageText txt", useDark && 'dark');
+          const isMine = message.authorId === authorId;
+          const isSelected = message.messageId === selectedMessage?.messageId;
           return (
             <li
               key={message.messageId}
-              className={message.authorId === authorId ? "mine" : "theirs"}
+              className={isMine ? "mine" : "theirs"}
             >
               <span className={className} style={style}>
                 {message.text}
               </span>
-              <MessageInteractions message={message} />
+              {
+                isSelected &&
+                <MessageInteractions message={message} reverse={isMine} />
+              }
             </li>
           );
         })}
