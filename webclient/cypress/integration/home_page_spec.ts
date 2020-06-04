66b4fcdb419f8fd85c892d91bf84237d6ee13a6f
jonathanmv <jonathan.morales@jimdo.com>
2020-06-04 07:05:05 +0200

    test: use forEach to test all ctas

--- webclient/cypress/integration/home_page_spec.ts
+++ webclient/cypress/integration/home_page_spec.ts
@@ -1,23 +1,16 @@
 describe('Home', () => {
-  it('should display ctas and they all take to a new room', () => {
-    cy.visit('/')
-    cy.contains('new chat')
-    cy.contains('Get Started')
-    cy.contains('Start for free')
+  ['new chat', 'Get Started', 'Start for free'].forEach(cta => {
+    it(`should display "${cta}" cta and take me to a new room`, () => {
+      cy.visit('/')
+      cy.contains(cta).click()
 
-    cy.contains('new chat')
-      .should('have.attr', 'href')
-      .then(href => {
-        cy.contains('new chat').click()
-        cy.url().should('include', href)
+      cy.get('input[type=text]')
+        .type('Hola')
+        .should('have.value', 'Hola')
+        .type('{enter}')
+        .should('have.value', '')
 
-        cy.get('input[type=text]')
-          .type('Hola')
-          .should('have.value', 'Hola')
-          .type('{enter}')
-          .should('have.value', '')
-
-        cy.contains('Hola')
-      })
+      cy.contains('Hola')
+    })
   })
 })
