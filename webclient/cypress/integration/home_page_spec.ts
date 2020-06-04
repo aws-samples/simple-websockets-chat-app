fa737ae60353566f4fd8f942d74ffd19aa2446ce
jonathanmv <jonathan.morales@jimdo.com>
2020-06-04 07:39:59 +0200

    test: that qr code loads

--- webclient/cypress/integration/home_page_spec.ts
+++ webclient/cypress/integration/home_page_spec.ts
@@ -4,13 +4,20 @@ describe('Home', () => {
       cy.visit('/')
       cy.contains(cta).click()
 
-      cy.get('input[type=text]')
+      cy.get('form input[type=text]')
         .type('Hola')
         .should('have.value', 'Hola')
         .type('{enter}')
         .should('have.value', '')
-
       cy.contains('Hola')
+
+      cy.reload()
+      cy.contains('Hola').should('not.exist')
+
+      cy.contains('Invite others')
+      cy.contains('Copy link') // Paste functionality can't be tested atm
+      cy.url()
+        .then(url => cy.get('img').should('have.attr', 'alt', url))
     })
   })
 })
