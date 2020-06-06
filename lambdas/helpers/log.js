d34dff882e0e99e2ca7070bca3662c9891dc4006
jonathanmv <jonathan.morales@jimdo.com>
2020-06-06 13:18:17 +0200

    feat: let logger be composable

--- lambdas/helpers/log.js
+++ lambdas/helpers/log.js
@@ -12,8 +12,20 @@ const LOG_ON = isLevelOn(INFO);
 const WARN_ON = isLevelOn(WARN);
 const ERROR_ON = true;
 
-exports.debug = (...args) => DEBUG_ON && console.debug(...args)
-exports.log = (...args) => LOG_ON && console.log(...args)
-exports.info = (...args) => INFO_ON && console.log(...args)
-exports.warn = (...args) => WARN_ON && console.warn(...args)
-exports.error = (...args) => ERROR_ON && console.error(...args)
+const prepend = (prefix, args) => {
+  if (!prefix) return args;
+
+  let pre = Array.isArray(prefix) ? prefix : [prefix];
+  return pre.concat(args);
+};
+
+const buildLogger = prefix => ({
+  debug: (...args) => DEBUG_ON && console.debug(...prepend(prefix, args)),
+  log: (...args) => LOG_ON && console.log(...prepend(prefix, args)),
+  info: (...args) => INFO_ON && console.log(...prepend(prefix, args)),
+  warn: (...args) => WARN_ON && console.warn(...prepend(prefix, args)),
+  error: (...args) => ERROR_ON && console.error(...prepend(prefix, args)),
+  buildLogger: (newPrefix) => buildLogger(prefix ? [newPrefix, prefix] : [newPrefix])
+})
+
+module.exports = buildLogger();
