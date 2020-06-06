f906d0f35344c783d0a6cba5f05096778e6ca24c
jonathanmv <jonathan.morales@jimdo.com>
2020-06-06 10:10:32 +0200

    feat: added log levels to log helper

--- lambdas/helpers/log.js
+++ lambdas/helpers/log.js
@@ -1,3 +1,19 @@
-const { DEBUG_MODE } = process.env;
+const { logLevel } = require('./config');
 
-module.exports = (...args) => DEBUG_MODE && console.log(...args);
+const DEBUG = 'DEBUG'
+const INFO = 'INFO'
+const WARN = 'WARN'
+const getPriority = level => [DEBUG, INFO, WARN].indexOf(level);
+const isLevelOn = level => getPriority(level) >= getPriority(logLevel);
+
+const DEBUG_ON = isLevelOn(DEBUG);
+const INFO_ON = isLevelOn(INFO);
+const LOG_ON = isLevelOn(INFO);
+const WARN_ON = isLevelOn(WARN);
+const ERROR_ON = true;
+
+exports.debug = (...args) => DEBUG_ON && console.debug(...args)
+exports.log = (...args) => LOG_ON && console.log(...args)
+exports.info = (...args) => INFO_ON && console.log(...args)
+exports.warn = (...args) => WARN_ON && console.warn(...args)
+exports.error = (...args) => ERROR_ON && console.error(...args)
