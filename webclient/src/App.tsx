75a74eb7402992a3396036d554b58c51c86d7df1
marc.palm <info@marc.palm.de>
2020-09-19 13:30:27 +0200

    feat(back-button): back button in chat

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -15,8 +15,13 @@ const App: React.FC<{ roomId: string }> = ({ roomId }) => {
   return (
     <RoomProvider authorId={authorId} roomId={roomId} peopleInRoom={0}>
       <div className="room">
-      <div style={{ width: "100%" }}>
-        <Link to="/o">Back</Link></div>
+        <div>
+          <Link to="/o">
+            <svg viewBox="0 0 24 24" fill="black" width="48px" height="48px"><path d="M0 0h24v24H0V0z" fill="none"/>
+              <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"/>
+            </svg>
+          </Link>
+        </div>
         <Chat />
       </div>
     </RoomProvider>
