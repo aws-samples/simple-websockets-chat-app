3a0b212574006e717a58e90df3e9799e3de517e7
marc.palm <info@marc.palm.de>
2020-09-26 06:20:46 +0200

    fix: remove unused files

--- webclient/src/api/message.ts
+++
@@ -1,6 +0,0 @@
-import { Message } from '../interfaces'
-import { emitEvent } from './eventEmitter'
-
-export const broadcastMessage = (connection: WebSocket, message: Message) => {
-  emitEvent(connection, 'MESSAGE_SENT', message);
-}
