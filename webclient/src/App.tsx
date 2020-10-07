121b9cdd8a7f105b6b779aac1fcc09f015f075e5
jonathanmv <jonathan.morales@jimdo.com>
2020-10-07 03:21:23 +0100

    do not display rooms link if has only 1 room

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -5,7 +5,7 @@ import * as React from 'react'
 import Chat from './components/Chat'
 
 import { RoomProvider } from './context/roomContext'
-import { getOrInitRoomData } from './store'
+import { getOrInitRoomData, getRoomIdsWithoutHome } from './store'
 import { Link } from 'react-router-dom'
 import { RoomSetupContext } from './context/roomSetupContext'
 
@@ -16,11 +16,13 @@ const App: React.FC<{ roomId: string }> = ({ roomId }) => {
     <RoomProvider authorId={authorId} authorName={authorName} roomId={roomId} peopleInRoom={0}>
       <div className="room">
         <div className="top-bar">
-          <Link to="/o">
-            <svg viewBox="0 0 24 24" fill="black" width="48px" height="48px"><path d="M0 0h24v24H0V0z" fill="none"/>
-              <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"/>
-            </svg>
-          </Link>
+          {getRoomIdsWithoutHome().length > 1 && (
+            <Link to="/o">
+              <svg viewBox="0 0 24 24" fill="black" width="48px" height="48px"><path d="M0 0h24v24H0V0z" fill="none"/>
+                <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z"/>
+              </svg>
+            </Link>
+        )}
           <h1>
             <RoomSetupContext.Consumer>
               {value => value.roomName || "dilo"}
